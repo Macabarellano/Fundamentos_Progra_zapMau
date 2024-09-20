e04b50e9460e73405dd3cbc27111b542be9d1496
Macabarellano <macabarellano@gmail.com>
2024-09-20 08:52:11 -0600

    001_Mau
    Subida de cambios

---
+++ P001_Hola_Mundo/P001_Hola_Mundo.cpp
@@ -0,0 +1,18 @@
+// P001_Hola_Mundo.cpp : 
+// Mauricio Cabrera
+// Este archivo contiene la función 
+// "main". La ejecución del programa 
+// comienza y termina ahí.
+// Hablaremos con el usuario mediante
+// STD::COUT << " ";
+
+#include <iostream>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+    //\n es un salto de linea
+    //std::endl;
+    std::cout << "Hello Stranger\n";
+}
